7547c6649aaea97546474c85b990a0637133d7b7
Dongjae Heo <lindsay.heo@gmail.com>
2023-07-03 11:09:11 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import data from "./data";
+import pluralize from "pluralize";
+
+const App = () => {
+  return <h2>Birthday Reminder - Starter</h2>;
+};
+export default App;
